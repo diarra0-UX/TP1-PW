a379bc59f6c38725ef942a6ff122f44ac6cb81c7
ChrisBOUONGOU <chrisbouk@outlook.fr>
2025-09-25 17:37:49 -0400

    petit ajout dans le fichier quiz.js

--- Tp1-JeuQuestionnaire-etu/js/classes/Quiz.js
+++ Tp1-JeuQuestionnaire-etu/js/classes/Quiz.js
@@ -27,7 +27,20 @@ class Quiz {
      *
      * @returns {string}
      */
-    determinerEncouragement() {
+    determinerEncouragement(score) {
+        if(score == 100){
+            return "A++";
+        }else if(score >= 90){
+            return "A";
+        }else if(score >= 80){
+            return "B";
+        }else if(score >= 70){
+            return "C";
+        }else if(score >= 60){
+            return "D";
+        }else{
+            return "F";
+        }
 
     }
 }
